97a40f74eb7c9cbab03c10b26227802a4e4ae478
0xchin <77933451+0xChin@users.noreply.github.com>
2025-11-13 13:16:25 -0300

    chore: improve deployment comments

--- src/script/DeployRevSharesUpgrader.s.sol
+++ src/script/DeployRevSharesUpgrader.s.sol
@@ -6,11 +6,13 @@ import {console} from "forge-std/console.sol";
 import {RevShareContractsUpgrader} from "src/RevShareContractsUpgrader.sol";
 
 /// @notice Deploys the RevShareContractsUpgrader contract.
+/// @dev Deployed at https://sepolia.etherscan.io/address/0x65d1b057EeFE204cAb3AC1607ba4b577eeA1515e
 /// @dev Usage:
 ///      forge script src/script/DeployRevSharesUpgrader.s.sol:DeployRevSharesUpgrader \
-///        --rpc-url $RPC_URL \
-///        --broadcast \
-///        --verify
+///          --rpc-url https://ethereum-sepolia.rpc.subquery.network/public \
+///          --broadcast \
+///          --verify --private-key $PRIVATE_KEY --verifier custom \
+///          --verifier-url 'https://api.etherscan.io/v2/api?chainid=11155111&apikey={$API_KEY}'
 contract DeployRevSharesUpgrader is Script {
     /// @notice Deploys the RevShareContractsUpgrader contract
     /// @return upgrader The deployed RevShareContractsUpgrader contract
