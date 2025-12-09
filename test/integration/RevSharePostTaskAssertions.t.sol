d60373317cb98b263c3fae82dc2629109ab77d5e
0xchin <77933451+0xChin@users.noreply.github.com>
2025-12-09 10:27:11 -0300

    refactor: change hardcoded variables to environment

--- test/integration/RevSharePostTaskAssertions.t.sol
+++ test/integration/RevSharePostTaskAssertions.t.sol
@@ -11,13 +11,17 @@ import {IntegrationBase} from "./IntegrationBase.t.sol";
 ///      - RPC_URL: L2 RPC URL to create fork
 ///      - OPTIMISM_PORTAL: Portal address for the chain
 ///      - MIN_WITHDRAWAL_AMOUNT: Min withdrawal amount for L1Withdrawer
+///      - L1_WITHDRAWAL_RECIPIENT: L1 withdrawal recipient address
+///      - WITHDRAWAL_GAS_LIMIT: Gas limit for withdrawals
 ///      - CHAIN_FEES_RECIPIENT: Chain fees recipient address
 /// @dev Example command:
 /// ```sh
 /// RPC_URL="https://mainnet.optimism.io" \
 /// OPTIMISM_PORTAL="0xbEb5Fc579115071764c7423A4f12eDde41f106Ed" \
-/// MIN_WITHDRAWAL_AMOUNT="350000" \
-/// CHAIN_FEES_RECIPIENT="0x0000000000000000000000000000000000000001" \
+/// MIN_WITHDRAWAL_AMOUNT="2000000000000000000" \
+/// L1_WITHDRAWAL_RECIPIENT="0xed9B99a703BaD32AC96FDdc313c0652e379251Fd" \
+/// WITHDRAWAL_GAS_LIMIT="800000" \
+/// CHAIN_FEES_RECIPIENT="0x16A27462B4D61BDD72CbBabd3E43e11791F7A28c" \
 /// forge test --match-contract RevSharePostTaskAssertionsTest
 /// ```
 contract RevSharePostTaskAssertionsTest is IntegrationBase {
@@ -27,11 +31,9 @@ contract RevSharePostTaskAssertionsTest is IntegrationBase {
     // Chain configuration from env vars
     address internal _portal;
     uint256 internal _minWithdrawalAmount;
+    address internal _l1WithdrawalRecipient;
+    uint32 internal _withdrawalGasLimit;
     address internal _chainFeesRecipient;
-
-    // Hardcoded defaults
-    uint32 internal constant WITHDRAWAL_GAS_LIMIT = 800000;
-    address internal constant L1_WITHDRAWAL_RECIPIENT = 0x0000000000000000000000000000000000000001;
 
     // Flag to track if env vars are set
     bool internal _isEnabled;
@@ -49,14 +51,19 @@ contract RevSharePostTaskAssertionsTest is IntegrationBase {
         string memory rpcUrl = vm.envOr("RPC_URL", string(""));
         _portal = vm.envOr("OPTIMISM_PORTAL", address(0));
         _minWithdrawalAmount = vm.envOr("MIN_WITHDRAWAL_AMOUNT", uint256(0));
+        _l1WithdrawalRecipient = vm.envOr("L1_WITHDRAWAL_RECIPIENT", address(0));
+        _withdrawalGasLimit = uint32(vm.envOr("WITHDRAWAL_GAS_LIMIT", uint256(0)));
         _chainFeesRecipient = vm.envOr("CHAIN_FEES_RECIPIENT", address(0));
 
         // Check if all required env vars are set
         bool hasRpcUrl = bytes(rpcUrl).length > 0;
         bool hasPortal = _portal != address(0);
+        bool hasL1WithdrawalRecipient = _l1WithdrawalRecipient != address(0);
+        bool hasWithdrawalGasLimit = _withdrawalGasLimit != 0;
         bool hasChainFeesRecipient = _chainFeesRecipient != address(0);
 
-        _isEnabled = hasRpcUrl && hasPortal && hasChainFeesRecipient;
+        _isEnabled =
+            hasRpcUrl && hasPortal && hasL1WithdrawalRecipient && hasWithdrawalGasLimit && hasChainFeesRecipient;
 
         if (_isEnabled) {
             _l2ForkId = vm.createFork(rpcUrl);
@@ -68,15 +75,15 @@ contract RevSharePostTaskAssertionsTest is IntegrationBase {
         vm.selectFork(_l2ForkId);
 
         address l1Withdrawer =
-            _computeL1WithdrawerAddress(_minWithdrawalAmount, L1_WITHDRAWAL_RECIPIENT, WITHDRAWAL_GAS_LIMIT);
+            _computeL1WithdrawerAddress(_minWithdrawalAmount, _l1WithdrawalRecipient, _withdrawalGasLimit);
         address revShareCalculator = _computeRevShareCalculatorAddress(l1Withdrawer, _chainFeesRecipient);
 
         _assertL2State(
             l1Withdrawer,
             revShareCalculator,
             _minWithdrawalAmount,
-            L1_WITHDRAWAL_RECIPIENT,
-            WITHDRAWAL_GAS_LIMIT,
+            _l1WithdrawalRecipient,
+            _withdrawalGasLimit,
             _chainFeesRecipient
         );
     }
@@ -88,7 +95,7 @@ contract RevSharePostTaskAssertionsTest is IntegrationBase {
 
         // Compute L1Withdrawer address
         address l1Withdrawer =
-            _computeL1WithdrawerAddress(_minWithdrawalAmount, L1_WITHDRAWAL_RECIPIENT, WITHDRAWAL_GAS_LIMIT);
+            _computeL1WithdrawerAddress(_minWithdrawalAmount, _l1WithdrawalRecipient, _withdrawalGasLimit);
 
         // Disburse fees and assert withdrawal
         // Expected L1Withdrawer share = 3 ether * 15% = 0.45 ether
@@ -97,6 +104,6 @@ contract RevSharePostTaskAssertionsTest is IntegrationBase {
         // https://github.com/ethereum-optimism/optimism/blob/f392d4b7e8bc5d1c8d38fcf19c8848764f8bee3b/packages/contracts-bedrock/src/L2/SuperchainRevSharesCalculator.sol#L67-L101
         uint256 expectedWithdrawalAmount = 0.45 ether;
 
-        _executeDisburseAndAssertWithdrawal(_l2ForkId, l1Withdrawer, L1_WITHDRAWAL_RECIPIENT, expectedWithdrawalAmount);
+        _executeDisburseAndAssertWithdrawal(_l2ForkId, l1Withdrawer, _l1WithdrawalRecipient, expectedWithdrawalAmount);
     }
 }
